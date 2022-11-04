1ea55e0928bc593205326b28dec426aa63e46c31
thomasFjorstad <thomas.fjorstad@bouvet.no>
2022-11-04 17:02:15 +0100

    Revert fake data check from api

--- src/api/countryApi.tsx
+++ src/api/countryApi.tsx
@@ -4,7 +4,7 @@ import { ICountryApi } from "../_models/country";
 const apiKey = "b8dd9e4822a13e930aaf74c38bd0dbca";
 
 export const fetchCountries = async (): Promise<ICountryApi[]> => {
-  return await fetch(`http://api.countrylayer.com/v2/all2?access_key=${apiKey}`)
+  return await fetch(`http://api.countrylayer.com/v2/all?access_key=${apiKey}`)
     .then((response) => response.json())
     .then((actualData) => {
       if (actualData.error) return fakeCountryList;
